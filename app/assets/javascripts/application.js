1ddfac89b739c0ae4a240cabc06ffb121c4a1194
Eyal Kedem <ekedem@gmail.com>
2014-06-11 15:43:06 +0300

    initial nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require bootstrap-player
 //= require_self
 
 $(document).ready(function() {
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   # named routes
-  
-  
+  match "/songs" => "pages#songs", :as => :songs, :via => :get
+  match "/song/:name" => "pages#song", :as => :song, :via => :get
+  match "/erela" => "pages#erela", :as => :erela, :via => :get
+
 end
