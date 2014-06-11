2d9f0cc3ada5f809419a3bc8c337d77dc1e91ec9
Eyal Kedem <ekedem@gmail.com>
2014-06-11 16:20:48 +0300

    integrating the songs data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private 
+
+  def songs_data
+    songs = [
+      image: 6,
+      url: "kochav",
+      music: "kochav_rough.mp3"
+    ]
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,13 @@ class PagesController < ApplicationController
   def index
     
   end
+
+  def songs
+    @songs = songs_data
+  end
+
+  def song
+    param_name = params[:name]
+    @song = songs_data.select {|v| v[:url] == param_name}.first
+  end
 end
